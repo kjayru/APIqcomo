2e8135a8bb3949118bb9ee8328b608d07cd9b45f
Wile <wiltinoco@gmail.com>
2018-10-10 02:02:09 -0500

    seguridad

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,7 +52,7 @@ class Handler extends ExceptionHandler
      public function render($request, Exception $exception)
     {
         $response = $this->handleException($request, $exception);
-        app(CorsService::class)->addActualRequestHeaders($response, $request);
+      
         return $response;
     }
     public function handleException($request, Exception $exception)
