dd1bcd91f9a21220442749e2bacbf9b24520c98f
kjayru <wiltinoco@gmail.com>
2019-03-19 19:53:19 -0500

    franqucias trasnform

--- app/Franchise.php
+++ app/Franchise.php
@@ -8,7 +8,7 @@ use App\Transformers\FranchiseTransformer;
 class Franchise extends Model
 {
     public $transformer = FranchiseTransformer::class;
-    protected $table = "franchises";
+    protected $table = "franchisees";
    
     protected $fillable = [
         'id','names','address', 'city', 'province','cellphone','mail', 'avatar','status','package_id','user_id','classification_id','created_at','updated_at'
