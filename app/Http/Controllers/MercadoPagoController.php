3462b333e248a04cc5e01856dd1e9e64ea3870d8
cristianty1086 <ctinocoy123@gmail.com>
2019-08-03 01:13:00 -0500

    mercado pago integracion 8

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -65,7 +65,7 @@ class MercadoPagoController extends Controller
   	];
     $preference = MP::post("/checkout/preferences",$preference_data); 
   */
-    return ['preference'=>$preference,'rpta'=>'ok','status'=>$preference->status]; 
+    return ['preference'=>$preference,'rpta'=>'ok']; 
   }
 
   public function onPaymentSuccess()
